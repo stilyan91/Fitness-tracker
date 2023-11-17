30a0ec6f616ac4cd7864900bf6b7329f041bd0a0
stilyan91 <stilyan91@gmail.com>
2023-11-17 23:35:16 +0200

    add utilis

---
+++ fitnes_tracker/accounts/utilis.py
@@ -0,0 +1,16 @@
+import requests
+import os
+class Edamam(object):
+    def __init__(self):
+        self.app_id = os.environ.get('API_id')
+        self.app_key = os.environ.get('API_key')
+
+    def get_food_data(self, food_name):
+        api_endpoint = "https://api.edamam.com/api/food-database/v2/parser"
+        payload = {
+            "ingr": food_name,
+            "app_id": self.app_id,
+            "app_key": self.app_key,
+        }
+        response = requests.get(api_endpoint, params=payload)
+        return response.json()
