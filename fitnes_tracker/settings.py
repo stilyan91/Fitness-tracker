739a189989929c5a3fa86ad475f49b7ebf477790
stilyan91 <stilyan91@gmail.com>
2023-11-18 09:08:36 +0200

    restore default

--- fitnes_tracker/settings.py
+++ fitnes_tracker/settings.py
@@ -67,9 +67,7 @@ DATABASES = {
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "HOST": os.environ.get('DB_HOST'),
         "PORT": os.environ.get('DB_PORT'),
-        "OPTIONS": {
-            "sslmode": "require",  # Use "require" if SSL/TLS is required.
-        },
+
     }
 }
 
